96e08f3c01a9a4f42904f9f06933f5b92c87f535
Anatoli Piven <sidtnlol@gmail.com>
2021-06-24 15:38:47 +0300

    change json and count rquests

--- get_data.py
+++ get_data.py
@@ -1,7 +1,9 @@
 import requests
 from collections import OrderedDict
+import time
 
 
+t = time.time()
 
 headers = {
         "accept": "application/json, text/javascript, */*; q=0.01",
@@ -10,7 +12,8 @@ headers = {
 }
 
 def get_data(index):
-    url = 'https://catalog.onliner.by/sdapi/catalog.api/search/videocard?page=1'
+    # url = 'https://catalog.onliner.by/sdapi/catalog.api/search/videocard?page=1'
+    url = 'https://catalog.onliner.by/sdapi/catalog.api/search/videocard?order=price:asc&page=1'
     r = requests.get(url=url, headers=headers)
 
     page = r.json()['page']
@@ -20,23 +23,31 @@ def get_data(index):
     dict_price = {}
 
     for page in range(1, page_count + 1):
-        url = f"https://catalog.onliner.by/sdapi/catalog.api/search/videocard?page={page}"
+        # url = f"https://catalog.onliner.by/sdapi/catalog.api/search/videocard?page={page}"
+        url = f'https://catalog.onliner.by/sdapi/catalog.api/search/videocard?order=price:asc&page={page}'
         r = requests.get(url=url, headers=headers)
         data = r.json()
+        count_pozition = 3
         products = data['products']
         for product in products:
-            if f' {index} ' in product['name']:
+            if f' {index} ' in product['name'] and len(dict_price) <= count_pozition - 1:
                 html_url = product['html_url']
                 product_name = product['extended_name']
-                if product['prices'] is None:
-                    continue
-                else:
-                    prices = product['prices']
-                    offers = prices['offers']
-                    count = offers['count']
-                    price_min = prices['price_min']
-                    amount = price_min['amount']
-                    dict_price[float(amount)] = f'{product_name}, {html_url}'
+                # if product['prices'] is None:
+                #     continue
+                # else:
+                prices = product['prices']
+                offers = prices['offers']
+                count = offers['count']
+                price_min = prices['price_min']
+                amount = price_min['amount']
+                dict_price[float(amount)] = f'{product_name}, {html_url}'
+                print(len(dict_price))
 
     dict_p = (OrderedDict(sorted(dict_price.items(), key=lambda t: t[0]))).items()
+    print(dict_price)
     return dict_p
+
+get_data('6800')
+
+print(time.time() - t)
